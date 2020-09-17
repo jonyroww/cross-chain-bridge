5bdc51a594627cea905d897df95c686ba38ce3c4
jonyroww <jonyroww@gmail.com>
2020-09-17 13:08:14 +0300

    fix(TransactionsBridgeController): uncomment value

--- src/transactions-bridge/transactions-bridge.controller.ts
+++ src/transactions-bridge/transactions-bridge.controller.ts
@@ -26,16 +26,16 @@ export class TransactionsBridgeController {
   ): Promise<CreateTransferResponseDto> {
     this.logger.log(`Transfer data: ${JSON.stringify(body)}`, 'transferTokens');
 
-    // const transferObject:
-    //   | TransactionDto
-    //   | { id: null } = await this.transactionsApiService
-    //   .transferTokens(body)
-    //   .catch(err => {
-    //     this.logger.error(err);
-    //     return { id: null };
-    //   });
+    const transferObject:
+      | TransactionDto
+      | { id: null } = await this.transactionsApiService
+      .transferTokens(body)
+      .catch(err => {
+        this.logger.error(err);
+        return { id: null };
+      });
 
-    // this.logger.log({ transferObject });
+    this.logger.log({ transferObject });
 
     const poolAddresses = await this.transactionsApiService.getPoolAddresses();
 
@@ -50,84 +50,50 @@ export class TransactionsBridgeController {
 
   @ApiOkResponse({ type: () => GetHistoryResponseDto })
   @Get()
-  getHistory(@Param() param: GetHistoryParamDto): GetHistoryResponseDto {
+  getHistory(
+    @Param() param: GetHistoryParamDto,
+  ): Promise<GetHistoryResponseDto> {
     this.logger.log(
       `History for address: ${JSON.stringify(param.address)}`,
       'getHistory',
     );
-    //return this.transactionsApiService.getHistory(param.address)
-    return {
-      status: 'Success',
-      transactions: [
-        {
-          id: '0c2a9ed4-412d-4666-b7f7-c369383b7b5d',
-          tx_in_hash:
-            '0x68df497a87d57595de94890916888e6c3ae8826df1624b22dc09e33e6b9393a3',
-          tx_out_hash:
-            '0x4880c44f3bba8bfda9ddd7cdb304d41fe202bb319ac28e86293367ef2915b3f5',
-          address_from: '0x6efad302e8Ed34157794FeA1508159e35fE1Aa85',
-          address_to: '0x6efad302e8Ed34157794FeA1508159e35fE1Aa85',
-          type_in: 'ETH',
-          type_out: 'BSC',
-          amount: '100',
-          createdAt: '2020-09-08T09:45:03.673Z',
-          updatedAt: '2020-09-08T09:45:03.673Z',
-          status: 'SUCCESS',
-        },
-      ],
-    };
+    return this.transactionsApiService.getHistory(param.address);
   }
 
   @ApiOkResponse({ type: () => TransferResponseDto })
   @Get(':transferId')
-  getTransferById(@Param('id') transferId: string): TransferResponseDto {
-    // const transfer = await this.transactionsApiService.getTransferById(
-    //   transferId,
-    // );
-    // return {
-    //   id: transfer.id,
-    //   txIn: {
-    //     amount: transfer.amount,
-    //     createdAt: transfer.createdAt,
-    //     status: transfer.status,
-    //     txHash: transfer.tx_in_hash,
-    //   },
-    //   txOut: {
-    //     amount: transfer.amount,
-    //     createdAt: transfer.createdAt,
-    //     status: transfer.status,
-    //     txHash: transfer.tx_out_hash,
-    //   },
-    // };
+  async getTransferById(
+    @Param('id') transferId: string,
+  ): Promise<TransferResponseDto> {
+    const transfer = await this.transactionsApiService.getTransferById(
+      transferId,
+    );
     return {
-      id: transferId,
+      id: transfer.id,
       txIn: {
-        amount: null,
-        createdAt: '2020-09-08T09:45:03.673Z',
-        status: 'PENDING',
-        txHash: null,
+        amount: transfer.amount,
+        createdAt: transfer.createdAt,
+        status: transfer.status,
+        txHash: transfer.tx_in_hash,
       },
       txOut: {
-        amount: null,
-        createdAt: '2020-09-08T09:45:03.673Z',
-        status: 'PENDING',
-        txHash: null,
+        amount: transfer.amount,
+        createdAt: transfer.createdAt,
+        status: transfer.status,
+        txHash: transfer.tx_out_hash,
       },
     };
   }
 
-  // @ApiOkResponse({ type: () => CheckStatusResponseDto })
-  // @Get('/transaction-status')
-  // checkTransactionStatus(
-  //   @Param() param: StatusCheckDto,
-  // ): CheckStatusResponseDto {
-  //   this.logger.log(
-  //     `Check status for: ${JSON.stringify(param)}`,
-  //     'checkTransactionStatus',
-  //   );
-  //   //return this.transactionsApiService.statusCheck(param);
-  //   return {
-  //     status: 'SUCCESS',
-  //   };
-  // }
+  @ApiOkResponse({ type: () => CheckStatusResponseDto })
+  @Get('/transaction-status')
+  checkTransactionStatus(
+    @Param() param: StatusCheckDto,
+  ): Promise<CheckStatusResponseDto> {
+    this.logger.log(
+      `Check status for: ${JSON.stringify(param)}`,
+      'checkTransactionStatus',
+    );
+    return this.transactionsApiService.statusCheck(param);
+  }
 }
